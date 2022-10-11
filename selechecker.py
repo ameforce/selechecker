ed938de107c7931285f47e95c56cea8bbfd1cde4
김종인 <enmsoftware@gmail.com>
2022-10-11 20:40:16 +0900

    feat: add chromedriver version check function

--- selechecker.py
+++ selechecker.py
@@ -1,16 +1,20 @@
-# 샘플 Python 스크립트입니다.
-
-# Shift+F10을(를) 눌러 실행하거나 내 코드로 바꿉니다.
-# 클래스, 파일, 도구 창, 액션 및 설정을 어디서나 검색하려면 Shift 두 번을(를) 누릅니다.
+import chromedriver_autoinstaller
+import os
 
 
-def print_hi(name):
-    # 스크립트를 디버그하려면 하단 코드 줄의 중단점을 사용합니다.
-    print(f'Hi, {name}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
+def driver_check():
+    driver_install_path = f'{os.getcwd()}\\lib\\chromedriver'
+    chrome_ver = chromedriver_autoinstaller.get_chrome_version().split('.')[0]
+    driver_path = f'{driver_install_path}\\{chrome_ver}\\chromedriver.exe'
 
-
-# 스크립트를 실행하려면 여백의 녹색 버튼을 누릅니다.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# https://www.jetbrains.com/help/pycharm/에서 PyCharm 도움말 참조
+    if os.path.exists(driver_path):
+        print(f'chrome driver is installed: {driver_path}')
+    else:
+        print(f'chrome driver is not installed.')
+        print(f'install the chrome driver(ver: {chrome_ver})')
+        if not os.path.exists(driver_install_path):
+            os.makedirs(driver_install_path)
+            while not os.path.exists(driver_install_path):
+                continue
+        chromedriver_autoinstaller.install(False, driver_install_path)
+    return driver_path
